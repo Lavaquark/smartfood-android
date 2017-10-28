ecee164c27b56ec171444e4817ff959039649b87
cypog <robin@hoechster.net>
2017-10-29 00:57:17 +0200

    Fix layout

--- app/src/main/java/com/hackathonstuttgart/smartfoodapp/smartfoodapp/MainActivity.java
+++ app/src/main/java/com/hackathonstuttgart/smartfoodapp/smartfoodapp/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateView()
     {
-        itemGrid.getchildre
+        //itemGrid.getchildre
     }
 
     private void loginFirebase()
